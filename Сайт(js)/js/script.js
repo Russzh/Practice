b57d1bf9db53ee9984fd0781a4eed0494cdc76f0
Russzh <47798938+Russzh@users.noreply.github.com>
2019-02-21 21:14:52 +0300

    Add files via upload

---
+++ Сайт(js)/js/script.js
@@ -0,0 +1,9 @@
+console.log(menu);
+menu.onclick = function	myFunction(){
+	var x	= document.getElementById("myTopnav");
+	if (x.className == "topnav"){
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
